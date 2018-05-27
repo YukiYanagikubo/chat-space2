a9862ba8236b4670ef40721f47682094fcc0c833
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-05-27 20:00:06 +0900

    automaticUpdating

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,10 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json { @new_messages = @messages.where('id > ?', params[:id]) }
+    end
   end
 
   def create
